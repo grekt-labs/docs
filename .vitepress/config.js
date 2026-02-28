21de19cda75c0c0f857ab92c23c1ef2a41658a9c
dygerydoo <caserofrancisco@gmail.com>
2026-02-28 10:59:09 +0100

    fix: replace GTM with gtag.js direct for GA4
    Switch from Google Tag Manager (GTM-57FHTDJK) to gtag.js direct
    (G-ZZTWBSWYM7). Consent mode v2 preserved. Remove noscript iframe
    that only applied to GTM.

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -17,8 +17,8 @@ export default defineConfig({
 
   head: [
     ['script', {}, "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('consent','default',{'analytics_storage':'denied','ad_storage':'denied','ad_user_data':'denied','ad_personalization':'denied'});"],
-    ['script', {}, "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-57FHTDJK');"],
-    ['noscript', {}, '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57FHTDJK" height="0" width="0" style="display:none;visibility:hidden"></iframe>'],
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-ZZTWBSWYM7' }],
+    ['script', {}, "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-ZZTWBSWYM7');"],
     ['link', { rel: 'icon', href: '/favicon.ico', sizes: '48x48' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
